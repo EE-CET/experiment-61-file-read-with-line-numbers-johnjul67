644f706178592c6ca67114f23327ecd3ff3baceb
John <johnjulious2006@gmail.com>
2026-04-09 16:48:12 +0530

    commit1
    Implemented file reading with line numbering and error handling.

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 public class FileLineReader {
     public static void main(String[] args) {
         String fileName = "input.txt";
-        
-        // TODO: Use a try-with-resources block to open a BufferedReader for the file
-        // TODO: Read the file line by line using a loop
-        // TODO: Keep track of the line number (starting from 1)
-        // TODO: Print each line in the format "lineNumber: lineContent"
-        // TODO: Catch and handle any IOExceptions that might occur
-        
-        
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
+            String line;
+            int lineNumber = 1;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(lineNumber + ": " + line);
+                lineNumber++;
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
     }
 }
